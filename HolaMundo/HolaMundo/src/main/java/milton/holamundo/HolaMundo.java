d5a680c2c41eb1e4bc7091746474f57f0c7f9f47
MR <MR@DESKTOP-SGF5D2F>
2019-09-18 15:39:46 -0500

    empesando de nuevo=0;

---
+++ HolaMundo/HolaMundo/src/main/java/milton/holamundo/HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package milton.holamundo;
+
+/**
+ *
+ * @author MR
+ */
+public class HolaMundo {
+    public static void main(String arg[]) {
+        
+    
+    System.out.println("hola mundo cruel jaja");        
+    
+
+}
+    
+}
