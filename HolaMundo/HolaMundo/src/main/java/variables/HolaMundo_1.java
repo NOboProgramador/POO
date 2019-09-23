c866360bb6903e9ad93f0ae877f1e16e4e85f72b
MR <MR@DESKTOP-SGF5D2F>
2019-09-22 19:41:53 -0500

    hay vamos poco a poco

---
+++ HolaMundo/HolaMundo/src/main/java/variables/HolaMundo_1.java
@@ -0,0 +1,16 @@
+package variables;
+public class HolaMundo_1 {
+
+    public static void main(String[] args) {
+
+        String saludosDesdeJava = "Hola desde Java";
+        System.out.println( saludosDesdeJava );
+        
+        int _holaDesde1Java;
+        float $adios;
+        char adios;
+        char a123dios;
+        
+        
+    }
+}
