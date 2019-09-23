c5cf9b5ae2a7929d81f63cc760705098fac4132e
MR <MR@DESKTOP-SGF5D2F>
2019-09-23 13:23:20 -0500

    hay vamos avanzando

---
+++ HolaMundo/HolaMundo/src/main/java/variables/primitivos/HolaMundo_2.java
@@ -0,0 +1,20 @@
+package variables.primitivos;
+import java.util.Scanner;
+
+public class HolaMundo_2 {
+
+    public static void main(String[] args) {
+
+        int numeroDecimal = 10;
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        float numeroOctal = 012;
+        System.out.println("numeroOctal = " + numeroOctal);
+        
+        char numeroHexadecimal = 0xA;
+        System.out.println("numeroHexadecimal = " + numeroHexadecimal);
+        
+        double numberoBinario = 0b1010;
+        System.out.println("numberoBinario = " + numberoBinario);
+    }
+}
